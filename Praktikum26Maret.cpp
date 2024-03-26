59659bb314c2ef5d27adbf582e6560d38be1122c
harlanfadhilah69 <harlancoegg@gmail.com>
2024-03-26 17:41:23 +0700

    Prosedur Input

--- Praktikum26Maret.cpp
+++ Praktikum26Maret.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+	cout << "Masukan bilangan Pertama = ";
+	cin >> bilA;
+	cout << "Masukan bilangan kedua = ";
+	cin >> bilB;
+
+}
